61b194d79d3b52e6ef4b3a210042cd0d40e4123d
nominik-spb <nominik.spb@gmail.com>
2025-09-10 16:44:23 +0300

    change

--- src/Randoms.java
+++ src/Randoms.java
@@ -4,16 +4,29 @@ import java.util.Random;
 public class Randoms implements Iterable<Integer> {
 
     protected Random random;
+    protected int min;
+    protected int max;
 
     public Randoms(int min, int max) {
-        max -= min;
         this.random = new Random();
+        this.min = min;
+        this.max = max;
     }
 
     @Override
     public Iterator<Integer> iterator() {
-        return null;
-    }
 
-    //...
+        return new Iterator<>() {
+
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return random.nextInt(max - min + 1) + min;
+            }
+        };
+    }
 }
